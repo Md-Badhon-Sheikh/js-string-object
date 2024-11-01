5310be7a50bc99d2e8051cfd17081f61612d14cf
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-11-01 20:40:44 +0600

    added file

---
+++ case.js
@@ -0,0 +1,6 @@
+const school = "Rover Polly High School";
+
+// console.log(school);
+// console.log(school.toUpperCase());
+// console.log(school.toLowerCase());
+
---
+++ string.js
@@ -0,0 +1,19 @@
+const name = "Abbas Ali";
+const fathersName = 'Kuddus Ali';
+const mothersName = `Amena Begum`;
+const son = new String("name")
+
+// console.log(typeof mothersName);
+// console.log(typeof son)
+// console.log(son);
+
+
+// Array is mutable 
+let arr = [2,5,6,8];
+
+arr [2]=10;
+console.log(arr);
+
+// string is immutable
+let friend = "Eja dur";
+// console.log(friend.length)
