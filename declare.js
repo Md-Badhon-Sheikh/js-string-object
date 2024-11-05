f7a2b435f8bb653e3329363b4101fddbaeaf0472
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-11-05 22:29:05 +0600

    added more files

---
+++ declare.js
@@ -0,0 +1,5 @@
+const pen = {brand: 'Matador', price: '5 tk', color: 'black'}
+const pencil = new Object();
+// console.log(pencil);
+const rubber = Object.create({});
+console.log(rubber);
---
+++ loop.js
@@ -0,0 +1,19 @@
+const mobile = {
+    brand: "Samsung",
+    price:250000,
+    color: 'black',
+    camera: '12mp',
+    isNew : true
+}
+
+for(const prop in mobile){
+    // console.log(prop);
+    // console.log(mobile[prop]);
+}
+
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    console.log(key, ':' ,mobile[key]);
+}
